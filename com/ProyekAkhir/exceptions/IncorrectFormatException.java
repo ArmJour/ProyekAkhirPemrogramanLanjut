0133c671dfc5af96ca367e19af5ec01fefea01ab
Raditya Arsyad <raditarsyad07@student.ub.ac.id>
2025-06-17 14:28:47 +0700

    Update IncorrectFormatException.java
    added the code

--- com/ProyekAkhir/exceptions/IncorrectFormatException.java
+++ com/ProyekAkhir/exceptions/IncorrectFormatException.java
@@ -1,5 +1,16 @@
 package com.ProyekAkhir.exceptions;
 
-public class IncorrectFormatException {
-    
+public class IncorrectFormatException extends Exception {
+    public IncorrectFormatException(String message) {
+        super(message);
+    }
+
+    public IncorrectFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    @Override
+    public String toString() {
+        return "ERROR IncorrectFormatException: " + getMessage();
+    }
 }
